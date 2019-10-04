4c1867807e49c1de21832163152a5c51bb7b0305
Philipp Tempel <git@philipptempel.me>
2019-10-04 08:38:49 +0200

    Update `Contents.m` files

--- Contents.m
+++ Contents.m
@@ -4,4 +4,3 @@
 %   finish    - shuts down the project
 %   projpath  - returns the path definiton for this project
 %   startup   - starts this project
-%   untitled2 - x = linspace(-1, 1, 100001);
