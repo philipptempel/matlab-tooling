8103b35c9dd18ee7e57523dab7aca87f03f3daee
Philipp Tempel <git@philipptempel.me>
2020-06-11 16:53:46 +0200

    Add submodule `solarized-matlab`

---
+++ sys/solarized.m
@@ -0,0 +1,25 @@
+function solarized(varargin)
+%% SOLARIZED 
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2020-06-11
+% Changelog:
+%   2020-06-11
+%       * Initial release
+
+
+
+%% Do your code magic here
+% just pass all arguments down to the `setupSolarized` function
+setupSolarized(varargin{:});
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header Your contribution towards improving this function
+% will be acknowledged in the "Changes" section of the header
