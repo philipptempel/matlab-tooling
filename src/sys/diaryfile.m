d0f1c0943e8a71363c6644e0e97fca41ea4a5116
Philipp Tempel <git@philipptempel.me>
2021-03-18 10:16:39 +0100

    Add `diaryfile` function

---
+++ src/sys/diaryfile.m
@@ -0,0 +1,29 @@
+function df = diaryfile()
+%% DIARYFILE Get the global diary file
+%
+% Outputs:
+%
+%   DF                  Char to the current diary file
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@ls2n.fr>
+% Date: 2021-03-18
+% Changelog:
+%   2021-03-18
+%       * Initial release
+
+
+
+%% Do your code magic here
+
+df = fullfile(mlproject([], fileparts(mfilename('fullpath'))), 'diaries', sprintf('%s.txt', datestr(now, 'yyyy-mm-dd')));
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header Your contribution towards improving this function
+% will be acknowledged in the "Changelog" section of the header
