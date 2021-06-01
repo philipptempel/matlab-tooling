775e8ecc813472d82fb010974f612201b3fa6638
Philipp Tempel <git@philipptempel.me>
2021-06-01 11:32:55 +0200

    Add function to create a new diary file

---
+++ src/sys/diarynew.m
@@ -0,0 +1,25 @@
+function diarynew()
+%% DIARYNEW Make a new diary file
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@ls2n.fr>
+% Date: 2021-04-30
+% Changelog:
+%   2021-04-30
+%       * Initial release
+
+
+
+%% Do your code magic here
+
+diary(diaryfile());
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header. Your contribution towards improving this function
+% will be acknowledged in the "Changelog" section of the header.
