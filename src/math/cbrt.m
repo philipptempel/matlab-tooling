3034cc12c453ede2387f4025e1077994f108e52c
Philipp Tempel <git@philipptempel.me>
2021-10-07 14:41:22 +0200

    Add function for cubic root of argument

---
+++ src/math/cbrt.m
@@ -0,0 +1,33 @@
+function v = cbrt(x)
+%% CBRT Cubic root of X
+%
+% Inputs:
+%
+%   X                   Description of argument X
+%
+% Outputs:
+%
+%   V                   Description of argument V
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@ls2n.fr>
+% Date: 2021-10-07
+% Changelog:
+%   2021-10-07
+%       * Initial release
+
+
+
+%% Do your code magic here
+
+v = x ^ (1/3);
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header. Your contribution towards improving this function
+% will be acknowledged in the "Changelog" section of the header.
