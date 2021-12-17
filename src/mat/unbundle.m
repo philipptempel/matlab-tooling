a5922905f8dbca3b5b04527b9fb8d36a649da698
Philipp Tempel <git@philipptempel.me>
2021-12-17 17:06:44 +0100

    Add support for `-1` as argument in unbundle

--- src/mat/unbundle.m
+++ src/mat/unbundle.m
@@ -1,17 +1,17 @@
 function varargout = unbundle(b, varargin)%#codegen
 %% UNBUNDLE Unbundle a bundled vector into separate entities
 %
-% [A, B, ...] = UNBUNDLE(B, DIMA, DIMB, ...) unbundles bundle vector B into
-% arrays A, B, etc of given dimensions DIMA, DIMB, etc.
+% [A1, A2, ...] = UNBUNDLE(B, DIMA1, DIMA2, ...) unbundles bundle vector B into
+% arrays A1, A2, etc of given dimensions DIMA1, DIMA2, etc.
 %
 % Inputs:
 %
 %   B                   Nx1 bundle vector of data
 %
-%   DIMA                Dimension(s) of the first array to extract from B. DIMA
-%                       can be a scalar R to extract a column vector of R rows
-%                       or a S-dimensional array of dimensions to reshape the
-%                       vector into.
+%   DIMAn               Dimension(s) of the first array to extract from B. DIMAn
+%                       can be a scalar R to extract a column vector of R rows,
+%                       an S-dimensional array of dimensions to reshape the
+%                       vector into, or -1 to extract everything until the end
 %
 % See also:
 %   BUNDLE
@@ -20,8 +20,11 @@ function varargout = unbundle(b, varargin)%#codegen
 
 %% File information
 % Author: Philipp Tempel <philipp.tempel@ls2n.fr>
-% Date: 2021-11-18
+% Date: 2021-12-17
 % Changelog:
+%   2021-12-17
+%       * Add support for negative values in `DIMAn` argument so that the user
+%       can also split a bundle into arrays of fixed length and variable length.
 %   2021-11-18
 %       * Rename from `UNPACK` to `UNBUNDLE`
 %   2021-11-09
@@ -34,16 +37,17 @@ function varargout = unbundle(b, varargin)%#codegen
 % UNBUNDLE(B, DIM1, ...)
 narginchk(2, Inf);
 
+% Get list of dimenions
+dims = varargin;
+% Count dimensions
+ndims = numel(dims);
+
 % [A, ...] = UNBUNDLE(B, DIMA, ...)
-nargoutchk(0, nargin - 1);
+nargoutchk(0, ndims);
 
 
 
 %% Process
-
-% Get list of dimenions
-dims = varargin;
-ndims = nargin - 1;
 
 % Create output
 varargout = cell(1, ndims);
@@ -51,15 +55,26 @@ varargout = cell(1, ndims);
 % Offset for linear indexing
 idxoff = 0;
 
-% Loop over each pack dimension to get
-for idim = 1:ndims
+% Initialize loop variables
+done = false;
+idim = 1;
+
+% Loop over all to-extract elements
+while ~done
   % Dimensions to get
   vdim = dims{idim};
   % Number of elements
   vnum = prod(vdim);
   
-  % Extract
-  v = b(idxoff + (1:vnum));
+  % Extract everything till the end
+  if vnum < 0
+    v = b((idxoff + 1):end);
+    
+  % Extract only a subset of data
+  else
+    v = b(idxoff + (1:vnum));
+    
+  end
   
   % Reshape into matrix
   if numel(vdim) > 1
@@ -71,6 +86,12 @@ for idim = 1:ndims
   
   % Udpate index offset
   idxoff = idxoff + vnum;
+  
+  % Increase loop counter
+  idim = idim + 1;
+  
+  % And evaluate if we are done or not
+  done = idim > ndims || vnum < 0;
   
 end
 
