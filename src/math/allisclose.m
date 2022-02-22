1a19095af8f98e6fce238cfa620905453b25ecdc
Philipp Tempel <git@philipptempel.me>
2022-02-22 10:51:19 +0100

    Change order of arguments in `allisclose` to match that of `isclose`

--- src/math/allisclose.m
+++ src/math/allisclose.m
@@ -1,21 +1,21 @@
-function f = allisclose(g, c, atol, rtol, varargin)%#codegen
+function f = allisclose(c, g, atol, rtol, varargin)%#codegen
 %% ALLISCLOSE Check if elements are close to each other
 %
-% ALLISCLOSE(G, C) checks if all elements of A are close to respective elements
+% ALLISCLOSE(C, G) checks if all elements of A are close to respective elements
 % of B. Here, "close to" means in the numeric sense and within floating point
 % precision.
 %
-% ALLISCLOSE(G, C, ATOL) determine closeness with absolute tolerance ATOL.
+% ALLISCLOSE(C, G, ATOL) determine closeness with absolute tolerance ATOL.
 %
-% ALLISCLOSE(G, C, ATOL, RTOL) determine closeness with relative tolerance RTOL.
+% ALLISCLOSE(C, G, ATOL, RTOL) determine closeness with relative tolerance RTOL.
 %
 % ALLISCLOSE(___, ...) passes additional options to underlying `ALL` call.
 %
 % Inputs:
 %
-%   G                   NxM array of ground truth values.
-%
 %   C                   NxM array of candidate values.
+%
+%   G                   NxM array of ground truth values.
 %
 %   ATOL                Absolute tolerance in comparing values. Defaults to
 %                       `1e4 * eps(class(G))`.
@@ -35,10 +35,12 @@ function f = allisclose(g, c, atol, rtol, varargin)%#codegen
 
 %% File information
 % Author: Philipp Tempel <philipp.tempel@ls2n.fr>
-% Date: 2021-11-17
+% Date: 2022-02-22
 % Changelog:
+%   2022-02-22
+%     * Update argument order to match that of `ISCLOSE`
 %   2021-11-17
-%       * Update H1 to correct format
+%     * Update H1 to correct format
 %   2021-11-04
 %     * Update H1 documentation to include defaults of `ATOL` and `RTOL`
 %   2021-04-23
@@ -53,19 +55,19 @@ function f = allisclose(g, c, atol, rtol, varargin)%#codegen
 
 %% Parse arguments
 
-% ISCLOSE(G, C);
-% ISCLOSE(G, C, ATOL);
-% ISCLOSE(G, C, ATOL, RTOL);
+% ISCLOSE(C, G);
+% ISCLOSE(C, G, ATOL);
+% ISCLOSE(C, G, ATOL, RTOL);
 narginchk(2, 4);
-% F = ISCLOSE(G, C, ...);
+% F = ISCLOSE(C, G, ...);
 nargoutchk(0, 1);
 
-% F = ISCLOSE(G, C)
+% F = ISCLOSE(C, G)
 if nargin < 3
   atol = [];
 end
 
-% F = ISCLOSE(G, C, ATOL)
+% F = ISCLOSE(C, G, ATOL)
 if nargin < 4
   rtol = [];
 end
@@ -74,7 +76,7 @@ end
 
 %% Compare
 
-f = all(isclose(g, c, atol, rtol), varargin{:});
+f = all(isclose(c, g, atol, rtol), varargin{:});
 
 while ~isscalar(f)
   f = all(f, varargin{:});
