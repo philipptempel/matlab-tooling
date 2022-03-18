c9742ffb07f6de5e1ba001c8faeab4053fe238b1
Philipp Tempel <git@philipptempel.me>
2022-03-18 19:41:57 +0100

    Update `git` function to be faster by caching git availability

--- src/sys/git.m
+++ src/sys/git.m
@@ -1,144 +1,224 @@
-function [cmdout, statout] =  git(varargin)
+function varargout = git(varargin)
+%% GIT
+% 
 % A thin MATLAB wrapper for Git.
 %
-%   Short instructions:
-%       Use this exactly as you would use the OS command-line verison of Git.
+% Short instructions:
+% Use this exactly as you would use the OS command-line verison of Git.
 %
-%   Long instructions are:
-%       This is not meant to be a comprehensive guide to the near-omnipotent
-%       Git SCM:
-%           http://git-scm.com/documentation
+% Long instructions:
+%   This is not meant to be a comprehensive guide to the near-omnipotent
+%   Git SCM: http://git-scm.com/documentation
 %
-%       Common MATLAB workflow:
+% Common MATLAB workflow:
 %
-%       % Creates initial repository tracking all files under some root
-%       % folder
-%       >> cd ~/
-%       >> git init
+% Creates initial repository tracking all files under some root folder
+% >> cd ~/
+% >> git init
 %
-%       % Shows changes made to all files in repo (none so far)
-%       >> git status
+% Shows changes made to all files in repo (none so far)
+% >> git status
 %
-%       % Create a new file and add some code
-%       >> edit foo.m
+% Create a new file and add some code
+% >> edit foo.m
 %
-%       % Check repo status, after new file created
-%       >> git status
+% Check repo status, after new file created
+% >> git status
 %
-%       % Stage/unstage files for commit
-%       >> git add foo.m          % Add file to repo or to stage
-%       >> git reset HEAD .       % To unstage your files from current commit area
+% Stage/unstage files for commit
+% >> git add foo.m          % Add file to repo or to stage
+% >> git reset HEAD .       % To unstage your files from current commit area
 %
-%       % Commit your changes to a new branch, with comments
-%       >> git commit -m 'Created new file, foo.m'
+% Commit your changes to a new branch, with comments
+% >> git commit -m 'Created new file, foo.m'
 %
-%       % Other useful commands (replace ellipses with appropriate args)
-%       >> git checkout ...       % To restore files to last commit
-%       >> git branch ...         % To create or move to another branch
-%       >> git diff ...           % See line-by-line changes
+% Other useful commands (replace ellipses with appropriate args)
+% >> git checkout ...       % To res[cmdout, statout]tore files to last commit
+% >> git branch ...         % To create or move to another branch
+% >> git diff ...           % See line-by-line changes
 %
-%   Useful resources:
-%       1. GitX: A visual interface for Git on the OS X client
-%       2. Github.com: Remote hosting for Git repos
-%       3. Git on Wikipedia: Further reading
-%
-% v0.1,     27 October 2010 -- MR: Initial support for OS X & Linux,
-%                               untested on PCs, but expected to work
-%
-% v0.2,     11 March 2011   -- TH: Support for PCs
-%
-% v0.3,     12 March 2011   -- MR: Fixed man pages hang bug using redirection
-%
-% v0.4,     20 November 2013-- TN: Searching for git in default directories,
-%                               returning results as variable
-%
-% v0.5,     22 January 2015 -- TP: Suppressed printing of ans before
-%                                  git command output
-%
-% v0.6,     26 January 2015 -- HG: Add path to git
-%
-% v0.7      09 April 2015   -- VF: If user requests it, return Git results
-%                              as a variable instead of displaying them.
-%
-% v0.8      24 Juli 2015    -- MvdL: Return status output
-%
-%
-% Contributors: (MR) Manu Raghavan
-%               (TH) Timothy Hansell
-%               (TN) Tassos Natsakis
-%               (TP) Tyler Parsons
-%               (HG) Han Geerligs
-%               (VF) Vadim Frolov
-%               (MvdL) Marcel van der Linden
-%
-orgpath=getenv('PATH');
-quit_function=0;
-try
-    % Test to see if git is installed
-    [status,~] = system('git --version');
-    % if git is in the path this will return a status of 0
-    % it will return a 1 only if the command is not found
-    % git command output
-    result = '';
-    if status
-        % Checking if git exists in the default installation folders (for
-        % Windows)
-        if ispc
-            search = ~isempty(dir('c:\Program Files\Git\bin\git.exe'));
-            searchx86 = ~isempty(dir('c:\Program Files (x86)\Git\bin\git.exe'));
-        else
-            search = 0;
-            searchx86 = 0;
-        end
-        if ~(search||searchx86)
-            % If git is NOT installed, then this should end the function.
-            result = sprintf('git is not installed\n%s\n',...
-                'Download it at http://git-scm.com/download');
-            quit_function=1; % set quit_function flag: only result is displayed
-        end
-    end
-    % if quit_function then only display message
-    if ~quit_function
-        % If git exists but the status is 1, then it means that it is
-        % not in the path. We should add the path
-        if status
-            if search
-                gitpath='c:\Program Files\Git\bin';
-            else if searchx86
-                    gitpath='c:\Program Files (x86)\Git\bin';
-                end
-            end
-            setenv('PATH',[gitpath pathsep orgpath]); % add path to git
-        end
-        % We can call the real git with the arguments
-        arguments = parse(varargin{:});
-        if ispc
-            prog = '';
-        else
-            prog = ' | cat';
-        end
-        if strcmp(arguments, 'commit ')
-            answer = inputdlg('Comments:','Commit''s comments');
-            arguments = [arguments '-m"' char(answer) '"'];
-        end
-        [status,result] = system(['git ',arguments,prog]);
-    end
-    if nargout >= 1
-        cmdout = strtrim(result);
-        statout = status;
+% Useful resources:
+% 1. GitX: A visual interface for Git on the OS X client
+% 2. Github.com: Remote hosting for Git repos
+% 3. Git on Wikipedia: Further reading[cmdout, statout]
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@ls2n.fr>
+% URL: https://mathworks.com/matlabcentral/fileexchange/29154-a-thin-matlab-wrapper-for-the-git-source-control-system
+% Date: 2022-03-18
+% Changelog:
+%   2022-03-18
+%       * Initial release based on FEX submission 29154
+
+
+
+%% Parse arguments
+
+% GIT()
+% GIT(ACTION)
+% GIT(ARGUMENT, ARGUMENT, ...)
+narginchk(0, Inf);
+
+% GIT(___)
+% CMDOUT = GIT(___)
+% [STATUS, CMDOUT] = GIT(___)
+nargoutchk(0, 2);
+
+
+
+%% Algorithm
+
+persistent mode
+
+if isempty(mode)
+  mode = '';
+end
+
+switch lower(mode)
+  case 'active'
+    % We can call the real git with the arguments
+    arguments = parseArguments(varargin{:});
+    
+    % Piping of data on UNIX systems
+    if isunix()
+      prog = ' | cat';
     else
-        % Display result instead of returning it
-        % to suppress output of ans
-        disp(result);
+      prog = '';
     end
-    % restore the original path
-    setenv(orgpath);
-catch
-    % restore the original path
-    setenv(orgpath);
+    
+    [status, result] = system([ 'git ', arguments , prog ]);
+    cmdout = strtrim(result);
+
+  case 'notfound'
+    throw( ...
+        MException( ...
+            'GIT:RUN:NotFound' ...
+          , [ ...
+            'GIT was not found on your system. Please go to ' ...
+            , '''https://git-scm.com'' and download and install it from there.' ...
+            ] ...
+        ) ...
+    );
+    
+  otherwise
+    try
+      % By default, GIT was not found on the machine
+      mode = 'notfound';
+      
+      % Run a version check command to see if GIT is installed
+      [status, ~] = system('git --version');
+      
+      % Git command not successfully executed
+      if status == 0
+        
+        % Find GIT on windows machines
+        if ispc()
+          search = { 'C:\Program Files (x86)\Git\bin' , 'c:\Program Files\Git\bin' };
+          
+        % Find GIT on unix machines
+        else
+            % Find all locations of GIT on the path
+            [status, cmdout] = system('which -a git');
+            
+            % `WHICH` returned something
+            if status == 0
+              % Search locations
+              search = strsplit(strtrim(cmdout), newline());
+              
+            % `WHICH` didn't find anything, so let's try some default paths
+            else
+              search = { '/usr/local/bin/git' , 'usr/bin/git' , '/bin/git' };
+              
+            end
+            
+        end
+        
+        % Loop through all search locations
+        nsearch = numel(search);
+        for isearch = 1:nsearch
+          % If GIT can be found at any of the locations...
+          if 2 == exist(search{isearch}, 'file')
+            % Add the enclosing directory to MATLAB's path
+            [gitdir, ~, ~] = fileparts(search{isearch});
+            % Add directory of GIT to PATH environment
+            setenv('PATH', [ gitdir , pathsep() , getenv('PATH') ]);
+            % Mark mode as active
+            mode = 'active';
+
+            % And break out of loop
+            break;
+
+          end
+
+        end
+        
+      % Git was found
+      else
+        mode = 'active';
+        
+      end
+      
+      if strcmp(mode, 'active')
+        % Now, recursively call GIT so that the actual command is being executed
+        [status, cmdout] = git(varargin{:});
+        
+      else
+        status = 1;
+        cmdout = 'Git not found';
+        
+      end
+      
+    catch me
+      mode = 'notfound';
+      
+      addCause( ...
+          MException( ...
+              'GIT:INIT:NotFound' ...
+            , [ ...
+              'GIT was not found on your system. Please go to ' ...
+              , '''https://git-scm.com'' and download and install it from there.' ...
+              ] ...
+            ) ...
+        , me ...
+      );
+      
+      rethrow(me);
+      
+    end
+    
 end
+
+% [STATUS, CMDOUT] = GIT(___)
+if nargout > 1
+  varargout = {status, cmdout};
+  
+% CMDOUT = GIT(___)
+elseif nargout == 1
+  varargout = {cmdout};
+
+% GIT(___)
+else
+  disp(cmdout);
+  
 end
-function space_delimited_list = parse(varargin)
-space_delimited_list = cell2mat(...
-    cellfun(@(s)([s,' ']),varargin,'UniformOutput',false));
+
+
 end
+
+
+function sdl = parseArguments(varargin)
+%% PARSEARGUMENTS
+
+
+
+sdl = cell2mat(cellfun(@(s) ([s ,' ']), varargin, 'UniformOutput', false));
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header. Your contribution towards improving this function
+% will be acknowledged in the "Changelog" section of the header.
