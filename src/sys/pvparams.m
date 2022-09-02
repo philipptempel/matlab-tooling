04cdb07f29ece3b65ea9455df06ff385af269c72
Philipp Tempel <git@philipptempel.me>
2022-09-02 09:01:25 +0200

    Replace `VARARGIN` by `ARGS` to reduce function call overhead

--- src/sys/pvparams.m
+++ src/sys/pvparams.m
@@ -1,24 +1,27 @@
-function [numargs, pvargs] = pvparams(varargin)
+function [numargs, pvargs] = pvparams(args)%#codegen
 %% PVPARAMS Split variable arguments into numeric and name/value pairs
 %
-%   [NUMARGS, PVARGS] = PVPARAMS(VARARGIN) splits the list of variable arguments
-%   into a list of numeric arguments NUMARGS and of name/value pairs PVARGS.
+% [NUMARGS, PVARGS] = PVPARAMS(ARGS) splits the list of variable arguments into
+% a list of numeric arguments NUMARGS and of name/value pairs PVARGS.
 %
-%   Outputs:
+% Outputs:
 %
 %   NUMARGS             Cell array of numeric args extracted from VARARGIN.
 %
 %   PVARGS              Cell array of name/value args extracted from VARARGIN.
 %
-%   See also
+% See also
 %   PARSEPARAMS
 
 
 
 %% File information
 % Author: Philipp Tempel <matlab@philipptempel.me>
-% Date: 2021-12-14
+% Date: 2022-09-02
 % Changelog:
+%   2022-09-02
+%       * Replace VARARGIN with ARGS so that we pass only one argument into this
+%       function which creates less overhead in calling the function
 %   2021-12-14
 %       * Update email address of Philipp Tempel
 %   2019-01-27
@@ -29,19 +32,21 @@ function [numargs, pvargs] = pvparams(varargin)
 %% Split stuff
 
 % Gather if argument is is a char or string
-idxChars = cellfun(@matlab.graphics.internal.isCharOrString, varargin);
+idxChars = cellfun(@matlab.graphics.internal.isCharOrString, args);
 
 % Find the first char or string argument
 charindx = find(idxChars);
 
 % None found => all numeric args
 if isempty(charindx)
-  numargs = varargin;
-  pvargs = varargin(1:0);
+  numargs = args;
+  pvargs = args(1:0);
+
 % Found one, split arguments
 else
-  numargs = varargin(1:charindx(1)-1);
-  pvargs = varargin(charindx(1):end);
+  numargs = args(1:(charindx(1) - 1));
+  pvargs = args(charindx(1):end);
+  
 end
 
 
