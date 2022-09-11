a463d618192f0ccb8b370a099432288e506d6bdf
Philipp Tempel <git@philipptempel.me>
2022-09-11 09:23:11 +0200

    Add a no-op function

---
+++ src/func/noop.m
@@ -0,0 +1,28 @@
+function noop(varargin)%#codegen
+%% NOOP A no-op function in MATLAB
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@ls2n.fr>
+% Date: 2022-09-11
+% Changelog:
+%   2022-09-11
+%       * Initial release
+
+
+
+%% Parse arguments
+
+
+
+%% Algorithm
+
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header. Your contribution towards improving this function
+% will be acknowledged in the "Changelog" section of the header.
